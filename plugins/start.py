7c8d0692f28ed4a11f27a513d5965cd055b734f0
Ken-Takiii <142348839+KombawaMinasah@users.noreply.github.com>
2023-11-20 18:46:59 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -19,7 +19,7 @@ from config import (
 from database.sql import add_user, full_userbase, query_msg
 from pyrogram import filters, Client
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked
-from pyrogram.types import InlineKeyboardMarkup, Message
+from pyrogram.types import InlineKeyboardButton, Message
 from pyrogram.enums import ParseMode
 
 from helper_func import decode, get_messages, subsall, subsch, subsgc, substai, subslg, substr, subsml
